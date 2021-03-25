5780cce25aa6a8518fe27e104dfea29f2cbcee70
Maxwell Montegary <78772889+MaxwellHenry@users.noreply.github.com>
2021-03-24 21:33:20 -0700

    Add files via upload
    Arcade Snake Wed 3/24/2021

--- action game/app.js
+++ action game/app.js
@@ -1,5 +1,11 @@
 
 let gameState = {
+
+  player: {
+    name: '',
+    points: 0,
+  },
+
   canvas: [
       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
@@ -41,23 +47,53 @@ let snake = {
     [10, 7],
     [10, 8],
   ],
-  nextDirection: [0, 0],
+  nextDirection: [1, 0],
 
-  head: [body[0]]
+  
 
 };
 
 
 
 let apple = {
-  location: [Math.floor(Math.random() * 30), Math.floor(Math.random() * 20)]
+  location: []
 }
 
+let game;
 
 function buildInitialState() {
   renderState();
   buildSnake();
   buildApple();
+  requestPlayerName()
+}
+
+function requestPlayerName() {
+  const playerNameForm = $(`
+    <form id = "player-name-form">
+      <input id="player-name-input"type="text"/>
+      <button>Save Name</button>
+    </form>`)
+  const playerNameElement = $('#player-name');
+  playerNameElement.append(playerNameForm);
+
+  $("#app").on('input', '#player-name-input', function(event){
+    event.preventDefault();
+    let name = $('#player-name-input').val();
+    gameState.player.name = name;
+
+  })
+
+  $("#app").on('submit', '#player-name-form', function(event){
+    event.preventDefault();
+    if (gameState.player.name.trim() !== ""){
+      $('#player-name-input').remove();
+      playerNameElement.text(gameState.player.name)
+
+      startGame()
+    }
+  })
+
 }
 
 // render the state
@@ -74,21 +110,47 @@ function renderState() {
     });
   });
 }
-
+/// BUILD SNAKE  ///
 function buildSnake() {
+ 
+  
+
+
   $(".segment").removeClass("snake");
   const snakeHead = snake.body[0];
   const snakeHeadX = snakeHead[0];
   const snakeHeadY = snakeHead[1];
 
-  const newSnakeHead = [
-    snakeHeadX + snake.nextDirection[0],
-    snakeHeadY + snake.nextDirection[1],
-  ];
+  const snakeTail = snake.body[snake.body.length - 1];
+  const snakeTailX = snakeTail[0];
+  const snakeTailY = snakeTail[1];
+
+
+  const newSnakeHeadX = snakeHeadX + snake.nextDirection[0];
+  const newSnakeHeadY = snakeHeadY + snake.nextDirection[1];
+  const newSnakeHead = [newSnakeHeadX, newSnakeHeadY];
+
+
+  if (snakeHead[0] === apple.location[0] && snakeHead[1] === apple.location[1]){
+    const newSnakeTailX = snake.nextDirection[0]
+    const newSnakeTailY = snake.nextDirection[1]
+    const newSnakeTail = [newSnakeTailX, newSnakeTailY];
+
+    snake.body.push(newSnakeTail)
+    gameState.player.points++
+    $('#score').text (gameState.player.points)
+    buildApple();
+  }
+
+  if (newSnakeHead[0] > 29 || newSnakeHead[0] < 0 || newSnakeHead[1] > 19 || newSnakeHead[1] < 0){
+    console.log("you lossefgrtfgds")
+    clearInterval(game)
+  }
 
   snake.body.unshift(newSnakeHead);
   snake.body.pop();
 
+  
   snake.body.forEach(function (coordinates) {
     const coordinateX = coordinates[0];
     const coordinateY = coordinates[1];
@@ -96,242 +158,90 @@ function buildSnake() {
     const segmentElement = $(
       `[data-x="${coordinateX}"][data-y="${coordinateY}"]`
     );
+
+    if (segmentElement.hasClass("snake")) {
+      console.log("you ndsakljbfdlkusabdlfikuasb")
+      clearInterval(game);
+    }
     segmentElement.addClass("snake");
   });
+
+
+
+  
+
+  
+
+
 }
 
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
 
+function onBoardClick() {
+  
   renderState(); // show the user the new state
 }
 
 $(".board").on("click", onBoardClick); // etc
-
+//////////TICK FUNCTION///////////////
 // refresh the screen in an interval
 function tick() {
   // this is an incremental change that happens to the state every time you update...
-
+  
   buildSnake();
 }
+///// SET INTERVAL /////
+game = setInterval(tick, 1000 / 10); // as close to 30 frames per second as possible
 
-setInterval(tick, 2000 / 30); // as close to 30 frames per second as possible
+
+function startGame() {
+  setInterval()
+}
+
+
+
+
+
 
 $(window).on("keydown", function (event) {
-  // here you might read which key was pressed and update the state accordingly
-  switch (event.which) {
-    case 37:
-      //left
-      snake.nextDirection = [0, -1];
-      break;
-    case 38:
-      //up
-      snake.nextDirection = [-1, 0];
-      break;
-    case 39:
-      //right
-      snake.nextDirection = [0, 1];
-      break;
-    case 40:
-      //down
-      snake.nextDirection = [1, 0];
-      break;
-    case 32:
-      snake.nextDirection = [0, 0];
-      break;
-    default:
-      break;
+  // left //
+  if (event.keyCode === 37 && snake.nextDirection[1] !== 1) {
+    snake.nextDirection = [0, -1];
+  }
+  // up //
+  if (event.keyCode === 38 && snake.nextDirection[0] !== 1) {
+    snake.nextDirection = [-1, 0];
+  }
+  // right //
+  if (event.keyCode === 39 && snake.nextDirection[1] !== -1) {
+    snake.nextDirection = [0, 1];
+  }
+  // down //
+  if (event.keyCode === 40 && snake.nextDirection[0] !== -1) {
+    snake.nextDirection = [1, 0];
   }
 });
 
 
-
-
-
+/////////// BUILD APPLE ////////////
 function buildApple() {
-  const location = apple.location;
-  const appleX = location[0];
-  const appleY = location[1];
+  $('.segment').removeClass("apple")
+
+
+
+  const location = [Math.floor(Math.random() * 30), Math.floor(Math.random() * 20)]
+  apple.location = location;
+  
+  const appleX = apple.location[0];
+  const appleY = apple.location[1];
   const appleElement = $(`[data-x="${appleX}"][data-y="${appleY}"]`);
   appleElement.addClass('apple');
 
-  if ($(`[data-x="${appleX}"][data-y="${appleY}"]`) !== snake.body) {
-    appleElement.addClass('apple')
-
-    
+  if (snake.body.indexOf(apple.location) > -1) {
+    buildApple();
   }
   else {
-    buildApple()
+    appleElement.addClass('apple')
   }
 
 }
-
-function eatApple(){
-  const location = apple.location;
-  const appleX = location[0];
-  const appleY = location[1];
-  const appleElement = $(`[data-x="${appleX}"][data-y="${appleY}"]`);
-  if appleElement === snake.body{
-    push
-  }
-    
-  
-}
-
-
-
-
-
-
 buildInitialState();
-
-
-
-
-
-
-// // Initial State
-// let gameState = {
-    
-//     canvas:[
-
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',],
-//         ['','','','','','','','','','','','','','','','','','','','',]
-//     ]
-// }
-
-// let snake = {
-//     body: [ [5,20], [5, 21], [5, 22], [5, 23] ],
-
-//     // 10,4  10,5   ...
-//     // 10,3  10,4   ...
-
-//     nextDirection: [0, -1]
-// }
-
-
-
-// function buildInitialState() {
-//     renderState()
-//     buildSnake()
-
-// }
-
-// // Render The State
-// function renderState() {
-//     // console.log('this is a test log')
-//     const canvasElement = $("#canvas");
-//     canvasElement.empty()
-//     gameState.canvas.forEach(function(row, rowIndex){
-//         row.forEach(function(segment, segmentIndex){
-//             const segmentElement = $(
-//                 `<div class="segment" data-x = "${rowIndex}" data-y = "${segmentIndex} ></div>`);
-//             canvasElement.append(segmentElement)
-
-//         })
-//     })
-
-// }
-
-// function buildSnake() {
-//     $(".segment").removeClass('snake')
-
-//     $("#canvas").empty
-
-//     const snakeHead = snake.body[0]
-//     const snakeHeadX = snakeHead[0]
-//     const snakeHeadY = snakeHead[1]
-
-    
-//     const newSnakeHeadX = snakeHeadX + snake.nextDirection[0]
-//     const newSnakeHeadY = snakeHead + snake.nextDirection[1]
-//     const newSnakeHead = [snakeHeadX, snakeHeadY]
-    
-//     snake.body.pop()
-//     snake.body.unshift(newSnakeHead)
-
-
-//     snake.body.forEach(function(coordinates){
-//         const coordinateX = coordinates[0]
-//         const coordinateY = coordinates[1]
-
-//         // snake.body[coordinateX][coordinateY] = 
-
-//         const segmentElement = $(`[data-x="${coordinateX}"] [data-y="${coordinateY}"]`)
-//         segmentElement.css('background-color', 'green')
-//     })
-// }
-
-
-
-// // Listeners
-// function onBoardClick() {
-//   // update state, maybe with another dozen or so helper functions...
-
-//   renderState() // show the user the new state
-// }
-
-// $('.board').on('click', onBoardClick); // etc
-
-
-// setInterval(tick,500)
-
-
-
-// // Refresh the screen at an interval
-// function tick() {
-//     buildSnake();
-    
-// }
-  
-// // setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
-  
-// $(window).on('keydown', function (event) {
-
-//     if (event.keyCode === 37) {
-//         nextDirection [0,-1]
-//     }
-//     if (event.keyCode === 38) {
-//         nextDirection [0,0]
-//     }
-//     if (event.keyCode === 39) {
-//         nextDirection [0,0]
-//     }
-//     if (event.keyCode === 40) {
-//         nextDirection [0,0]
-//     }
-
-
-// });
-
-// buildInitialState()
-
